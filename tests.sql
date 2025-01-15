381cfb531d29ee9ca1d21c2a2e455948afd5956f
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2025-01-15 11:53:37 +0000

    Update tests.sql

--- tests.sql
+++ tests.sql
@@ -17,6 +17,11 @@ FROM `data-lake-prd-314410.cz.pull-pesquisas`
 WHERE TIMESTAMP_TRUNC(Data, DAY) BETWEEN TIMESTAMP("2020-01-01") AND TIMESTAMP("2020-12-31") AND Reservas IS NOT NULL
 AND Cidade_ID = 2851556;
 
+SELECT *
+FROM `data-lake-prd-314410.cz.pull-pesquisas`
+WHERE TIMESTAMP_TRUNC(Data, DAY) BETWEEN TIMESTAMP("2023-07-01") AND TIMESTAMP("2023-12-31") AND Reservas IS NOT NULL
+AND Cidade_ID = 2851556;
+
 
 SELECT COUNT(*)
 FROM `data-lake-prd-314410.cz.pull-pesquisas-canal-hotel`
