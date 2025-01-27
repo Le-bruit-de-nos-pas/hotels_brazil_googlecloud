53e05ff9385d6c04a8cebfa626cdf3b01dcd42eb
Paulo Bastos <91428363+Le-bruit-de-nos-pas@users.noreply.github.com>
2025-01-27 11:59:32 +0000

    Update tests.sql

--- tests.sql
+++ tests.sql
@@ -1,3 +1,7 @@
+
+SELECT COUNT(DISTINCT RequestID) AS distinct_request_count
+FROM `data-lake-prd-314410.rz.pull-01-request`;
+
 SELECT COUNT(DISTINCT RequestID) AS distinct_request_count
 FROM `data-lake-prd-314410.rz.pull-01-response`; -- 28311644
 
